6a08afa92ad9f35b1b28989398eb80674fac8f6b
Git-Javi <pruebasfpcastelao@gmail.com>
2024-04-21 14:11:18 +0200

    init commit

---
+++ src/main/java/com/jagasa/SbRestCrudOpenapiHexaApplication.java
@@ -0,0 +1,13 @@
+package com.jagasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRestCrudOpenapiHexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRestCrudOpenapiHexaApplication.class, args);
+	}
+
+}
